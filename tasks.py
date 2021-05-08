6b1eb057caf2dba4a22c5ff3109b7b927acc6484
Eden <edenden1@gmail.com>
2021-05-08 23:42:10 +0300

    - tasks.py (fixed constraints and added minimizer test(ep2))

--- tasks.py
+++ tasks.py
@@ -173,8 +173,8 @@ def get_model(real_to_observed, w, x, N):
 
 
 def dunkel():
-    a = 6.0/5.0
-    b = 6.0
+    a = 3
+    b = 1.5
 
     real_to_observed = {0: 0,
                         1: 1,
@@ -186,6 +186,11 @@ def dunkel():
                   [0, -a - b, b, a],
                   [a, a, -2 * b, 0],
                   [b, b, 0, -2 * a]], dtype=np.float)
+
+    # w = np.array([[-11, 2, 0, 1],
+    #               [3, -52.2, 2, 35],
+    #               [0, 50, -77, 0.7],
+    #               [8, 0.2, 75, -36.7]], dtype=np.float)
 
 
     model = Model(real_to_observed, w, 0.0001)
@@ -210,6 +215,27 @@ def entropy_production(n):
     return np.sum((n_jI-n_Ij)*np.log(n_jI/n_Ij) + (n_jK-n_Kj)*np.log(n_jK/n_Kj))
 
 
+def ep2(x):
+    a = x[0]
+    b = x[1]
+    return 4*(a-b)*np.log(a/b)
+
+
+def dunkel_exmple():
+    p_list = np.linspace(0, 0.99, 100)
+    ep_list = []
+    cons = [{'type': 'eq', 'fun': lambda x: np.sum(np.square(x)) / (np.sum(x)) - p},
+            {'type': 'ineq', 'fun': lambda x: 1 - np.sum(x)}
+            ]
+    bnds = tuple([(1e-5, None)] * 2)
+    for p in p_list:
+        x_0 = 0.01 * np.random.rand(2) + 0.25
+        res = minimize(ep2, x_0, method='SLSQP', options={'disp': True}, bounds=bnds, constraints=cons)
+        ep_list.append(ep2(res.x))
+    plt.plot(p_list, ep_list)
+    plt.show()
+
+
 if __name__ == '__main__':
     # task1()
     # task2()
@@ -228,15 +254,17 @@ if __name__ == '__main__':
     n = w.T*p
     n_IJK = trj._get_n_IJK(0, 2, 1)
     n_KJI = trj._get_n_IJK(1, 2, 0)
-    n_0 = np.zeros((1, 16))
-    cons = [{'type': 'eq', 'fun': lambda x: np.sum(x[4:8]*x[8:12]/(x[:4]+x[4:8]))-n_IJK},
-            {'type': 'eq', 'fun': lambda x: np.sum(x[:4]*x[12:]/(x[8:12]+x[12:]))-n_KJI},
+    n_0 = 0.25+0.1*np.random.rand(16)
+    # n_0 = np.append(n_0, n_0)
+    cons = [{'type': 'eq', 'fun': lambda x: np.sum(x[4:8]*x[8:12]/(x[4:8]+x[8:12]))-n_IJK},
+            {'type': 'eq', 'fun': lambda x: np.sum(x[:4]*x[12:]/(x[:4]+x[12:]))-n_KJI},
             {'type': 'ineq', 'fun': lambda x: n_est[2, 0] - np.sum(x[:4])},
             {'type': 'ineq', 'fun': lambda x: n_est[0, 2] - np.sum(x[4:8])},
             {'type': 'ineq', 'fun': lambda x: n_est[2, 1] - np.sum(x[8:12])},
             {'type': 'ineq', 'fun': lambda x: n_est[1, 2] - np.sum(x[12:])}
-            ]  # + [{'type': 'eq', 'fun': lambda x: x[i]+x[4+i]-x[8+i]-x[12+i]} for i in range(4)]
+            ] #+ [{'type': 'eq', 'fun': lambda x: x[i]+x[4+i]-x[8+i]-x[12+i]} for i in range(4)]
     bnds = tuple([(0.0001, None)]*16)
-    res = minimize(entropy_production, n_0, method='SLSQP', bounds=bnds, constraints=cons)
+    res = minimize(entropy_production, n_0, method='SLSQP', options={'disp': True}, bounds=bnds, constraints=cons)
     ep = entropy_production(res.x)
+
     pass
